6196fb8e75469ec9745f6133e0f26902ca7bb408
tanakorn128 <60030041@kmitl.ac.th>
2018-09-07 14:00:57 +0700

    update

---
+++ Lab03/Lab03/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello");
+            Console.Write("Hello");
+        }
+    }
+}
