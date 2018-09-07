ca6de7c5cda2271ec4a298c9fbf517bd40cd3799
tanakorn128 <60030041@kmitl.ac.th>
2018-09-07 14:36:53 +0700

    update

---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double i = 123.456789d;
+            Console.WriteLine("{0:F1}", i);
+            Console.WriteLine("{0:F2}", i);
+            Console.WriteLine("{0:F3}", i);
+            Console.WriteLine("{0:F4}", i);
+            Console.WriteLine("{0:F5}", i);
+
+        }
+    }
+}
