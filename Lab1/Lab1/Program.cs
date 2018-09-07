ff6b2f9424c1372b4beed78994bb47d08005da55
tanakorn128 <60030041@kmitl.ac.th>
2018-09-07 14:50:13 +0700

    9

--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -10,7 +10,7 @@ namespace Lab1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("The value: {0:C}.", 500);
+            Console.WriteLine("{0,-10:F4}", 12.3456789);
         }
     }
 }
